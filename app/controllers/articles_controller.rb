85a8c7666e75afc62a1f7d6b306ec2a9c9bc6205
Marshall Houston <marshall.houston@gmail.com>
2017-10-15 10:59:47 -0600

    add edit functionality and add flash.notice functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:destroy, :edit, :update]
+
 
   def index
     @articles = Article.all
@@ -15,12 +17,27 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+
+    flash.notice = "Article '#{@article.title}' created!"
     redirect_to article_path(@article)
   end
 
   def destroy
-    Article.destroy(params[:id])
+    @article.destroy
+    flash.notice = "Article '#{@article.title}' deleted!"
     redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' updated!"
+
+    redirect_to article_path(@article)
   end
 
   private
@@ -29,4 +46,7 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :body)
   end
 
+  def set_article
+    @article = Article.find(params[:id])
+  end
 end
